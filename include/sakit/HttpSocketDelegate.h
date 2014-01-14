5f75c450d3555ed54ceacb49e3a211e8a2fe33d4
Boris Mikic <boris.blizzard@gmail.com>
2014-01-14 11:57:39 +0000

    - added dummy HttpSocketDelegate

---
+++ include/sakit/HttpSocketDelegate.h
@@ -0,0 +1,35 @@
+/// @file
+/// @author  Boris Mikic
+/// @version 1.0
+/// 
+/// @section LICENSE
+/// 
+/// This program is free software; you can redistribute it and/or modify it under
+/// the terms of the BSD license: http://www.opensource.org/licenses/bsd-license.php
+/// 
+/// @section DESCRIPTION
+/// 
+/// Defines a HTTP socket delegate.
+
+#ifndef SAKIT_HTTP_SOCKET_DELEGATE_H
+#define SAKIT_HTTP_SOCKET_DELEGATE_H
+
+#include <hltypes/hstream.h>
+
+#include "sakitExport.h"
+#include "ServerDelegate.h"
+
+namespace sakit
+{
+	class HttpSocket;
+
+	class sakitExport HttpSocketDelegate
+	{
+	public:
+		HttpSocketDelegate();
+		virtual ~HttpSocketDelegate();
+
+	};
+
+}
+#endif
---
+++ src/HttpSocketDelegate.cpp
@@ -0,0 +1,22 @@
+/// @file
+/// @author  Boris Mikic
+/// @version 1.0
+/// 
+/// @section LICENSE
+/// 
+/// This program is free software; you can redistribute it and/or modify it under
+/// the terms of the BSD license: http://www.opensource.org/licenses/bsd-license.php
+
+#include "HttpSocketDelegate.h"
+
+namespace sakit
+{
+	HttpSocketDelegate::HttpSocketDelegate()
+	{
+	}
+
+	HttpSocketDelegate::~HttpSocketDelegate()
+	{
+	}
+
+}
