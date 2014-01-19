f8874140db5e47e5c7384e2107225a6b9c8e5fb1
Boris Mikic <boris.blizzard@gmail.com>
2014-01-19 14:32:01 +0000

    - small update

--- include/sakit/HttpSocketDelegate.h
+++ include/sakit/HttpSocketDelegate.h
@@ -29,8 +29,8 @@ namespace sakit
 		HttpSocketDelegate();
 		virtual ~HttpSocketDelegate();
 
-		virtual void onExecuteCompleted(sakit::HttpSocket* socket, sakit::HttpResponse* response, sakit::Url url) = 0;
-		virtual void onExecuteFailed(sakit::HttpSocket* socket, sakit::HttpResponse* response, sakit::Url url) = 0;
+		virtual void onExecuteCompleted(HttpSocket* socket, HttpResponse* response, Url url) = 0;
+		virtual void onExecuteFailed(HttpSocket* socket, HttpResponse* response, Url url) = 0;
 
 	};
 
