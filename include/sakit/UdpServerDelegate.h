ada55af0455aea9355d486bfeb65bdc8518ead8f
Boris Mikic <boris.blizzard@gmail.com>
2014-04-03 12:07:02 +0000

    - changed license to BSD-3

--- include/sakit/UdpServerDelegate.h
+++ include/sakit/UdpServerDelegate.h
@@ -5,7 +5,7 @@
 /// @section LICENSE
 /// 
 /// This program is free software; you can redistribute it and/or modify it under
-/// the terms of the BSD license: http://www.opensource.org/licenses/bsd-license.php
+/// the terms of the BSD license: http://opensource.org/licenses/BSD-3-Clause
 /// 
 /// @section DESCRIPTION
 /// 
