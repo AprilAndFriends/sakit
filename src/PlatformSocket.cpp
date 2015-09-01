ca2fc2539da8ebb254a1597748c5866eeaf3d167
Boris Mikic <boris.blizzard@gmail.com>
2015-09-01 21:34:09 +0200

    - hltypes update

--- src/PlatformSocket.cpp
+++ src/PlatformSocket.cpp
@@ -6,6 +6,7 @@
 /// This program is free software; you can redistribute it and/or modify it under
 /// the terms of the BSD license: http://opensource.org/licenses/BSD-3-Clause
 
+#define __HL_INCLUDE_PLATFORM_HEADERS
 #include <hltypes/hlog.h>
 #include <hltypes/hplatform.h>
 #include <hltypes/hstring.h>
